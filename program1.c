78d8d5fd092766faf3534a6b186ba435c6ccf682
oxygen1u8 <egors5779@gmail.com>
2021-10-26 21:29:06 +0300

    Added SIGINT event handler in first program and the third program are handle it

--- program1.c
+++ program1.c
@@ -14,80 +14,89 @@
 #define NO_INPUT_ARGS 1
 #define FILE_DOESNT_EXIST 2
 
-void sigint_event(int fifo);
+void sigint_event(int signum);						// функция, обрабатывающая сигнал SIGINT(Ctrl+C на клавиатуре вызывает его)
+
+
+
+void (*funcptr)(int);							// ниже пояснение
 
 int main(int argc, char* argv[])
 {
-    char buff[2]; // переменная для считывания данных из gpio17
-    char time_buff[50]; // массив символов, в котором будет храниться время, в которое произошёл хлопок
-    bool prev = 0, curr = 0; // булевые переменные для системы прерываний записи в файл/выводе в именованный канал
-    time_t ttime; // переменная, в которую будет записываться время хлопка
-    int fd_fifo; // дескриптор именованного канала
-    FILE* fp = NULL; // файл, название которого указано в argv[1]
-    FILE* in = NULL; // файл со значением в gpio17
+    char buff[2]; 							// переменная для считывания данных из gpio17
+    char time_buff[50]; 						// массив символов, в котором будет храниться время, в которое произошёл хлопок
+    bool prev = 0, curr = 0; 						// булевые переменные для системы прерываний записи в файл/выводе в именованный канал
+    time_t ttime; 							// переменная, в которую будет записываться время хлопка
+    int fd_fifo; 							// дескриптор именованного канала
+    FILE* fp = NULL; 							// файл, название которого указано в argv[1]
+    FILE* in = NULL; 							// файл со значением в gpio17
 
+    int a = 0;
     struct sigaction sa;
     sa.sa_handler = &sigint_event;
-    sa.sa_flags = SA_RESTART;
+    //sa.sa_flags = SA_RESTART;
 
     sigaction(SIGINT, &sa, NULL);
 
-    if(argc != 2) // обрабатывание ошибки об отсутствии входного аргумента
+    if(argc != 2) 							// обрабатывание ошибки об отсутствии входного аргумента
     {
 	fprintf(stderr, "No input arguments!\n");
         return NO_INPUT_ARGS;
     }
-    unlink("fifo"); // удаляем канал fifo, если был
-    if(mkfifo("fifo", 0777) == -1) // создание именованного канала и проверка создался ли, вывод о соответствующей ошибке
+    unlink("fifo"); 							// удаляем канал fifo, если был
+    if(mkfifo("fifo", 0777) == -1) 					// создание именованного канала и проверка создался ли, вывод о соответствующей ошибке
 	fprintf(stderr, "Can't create fifo!\n");
     while(1)
     {
-        //in = fopen("/sys/class/gpio/gpio17/value", "r"); // то же самое, что и ниже, только для raspberry pi
-	in = fopen("/home/oxygen1u8/gpio17/value", "r"); // открываем на чтение файл со значением в gpio17
-        fgets(buff, 2, in); // считывание из файла
-        if(atoi(buff) == 1) // если в файле была 1
+        //in = fopen("/sys/class/gpio/gpio17/value", "r"); 		// то же самое, что и ниже, только для raspberry pi
+	in = fopen("/home/oxygen1u8/gpio17/value", "r"); 		// открываем на чтение файл со значением в gpio17
+        fgets(buff, 2, in); 						// считывание из файла
+        if(atoi(buff) == 1) 						// если в файле была 1
         {
-	    curr = 1; // curr - текущее состояние, присваиваем 1
-            ttime = time(NULL); // замеряем время, в которое прозвучал хлопок
-	    if(curr != prev) // если предыдущее состояние gpio17 не равно нынешнему(если предыдущее 0), выводим в файл и именованный канал
+	    curr = 1; 							// curr - текущее состояние, присваиваем 1
+            ttime = time(NULL); 					// замеряем время, в которое прозвучал хлопок
+	    if(curr != prev) 						// если предыдущее состояние gpio17 не равно нынешнему(если предыдущее 0), выводим в файл и именованный канал
 	    {
-		fd_fifo = open("fifo", O_RDWR); // открываем канал на чтение и запись
-		if(fd_fifo == -1) // если не открылся, выводим ошибку
+		fd_fifo = open("fifo", O_RDWR); 			// открываем канал на чтение и запись
+		if(fd_fifo == -1) 					// если не открылся, выводим ошибку
 		    fprintf(stderr, "Can't open fifo!\n");
-		sprintf(time_buff, "%ld", ttime); // записываем время хлопка в time_buff
-		if(write(fd_fifo, time_buff, strlen(time_buff)) == -1) // запись в именованный канал и тут же проверка, записалось ли
-		    fprintf(stderr, "Not writing to fifo!\n"); // уведомление об ошибке
-		close(fd_fifo); // закрываем канал
-            	fp = fopen(argv[1], "a"); // открываем файл
-		if(fp == NULL) // если не открылся
+		sprintf(time_buff, "%ld", ttime); 			// записываем время хлопка в time_buff
+		if(write(fd_fifo, time_buff, strlen(time_buff)) == -1) 	// запись в именованный канал и тут же проверка, записалось ли
+		    fprintf(stderr, "Not writing to fifo!\n"); 		// уведомление об ошибке
+		close(fd_fifo); 					// закрываем канал
+            	fp = fopen(argv[1], "a"); 				// открываем файл
+		if(fp == NULL) 						// если не открылся
 		{
 		    fprintf(stderr, "File doesn't exist!\n");
-		    return FILE_DOESNT_EXIST; // вывод кода ошибки и завершение программы
+		    return FILE_DOESNT_EXIST; 				// вывод кода ошибки и завершение программы
 		}
-            	fprintf(fp, "%s\n", time_buff); // запись в файл
-            	fclose(fp); // закрываем файл
-            	fp = NULL; // указатель на файл присваиваем к NULL
+            	fprintf(fp, "%s\n", time_buff); 			// запись в файл
+            	fclose(fp); 						// закрываем файл
+            	fp = NULL; 						// указатель на файл присваиваем к NULL
 	    }
-	    prev = 1; // prev - предыдущее состояние gpio17, присваиваем 1
+	    prev = 1; 							// prev - предыдущее состояние gpio17, присваиваем 1
         }
 	else
-	    curr = prev = 0; // если в gpio17, нынешнее и предыдущее - нули
-        fclose(in); // закрываем файл gpio17
-        in = NULL; // присваиваем NULL
+	    curr = prev = 0; 						// если в gpio17, нынешнее и предыдущее - нули
+        fclose(in); 							// закрываем файл gpio17
+        in = NULL; 							// присваиваем NULL
     }
     return 0;
 }
 
-void sigint_event(int fifo)
+void sigint_event(int signum)
 {
-    int fd_fifo = open("fifo", O_RDWR);
-    char buff[3] = "-1\0", buff2[10];
-    if(fd_fifo == -1)
+    signal(SIGINT, funcptr);						// переприсваиваем обработчик сигнала SIGINT на стандартный (чтоб вызывалось завершение приложения)
+
+    mkfifo("fappexit", 0777);						// создаем канал, в котором сообщим о закрытии приложения
+
+    int fd_fifo = open("fappexit", O_RDWR);				// открываем его
+    char buff[3] = "-1";						// код закрытия программы (можно было любой, но я так захотел)
+    if(fd_fifo == -1)							// если канал не открылся, выводим сообщение об ошибке
     {
 	fprintf(stderr, "Can't open fifo\n");
 	return;
     }
-    read(fd_fifo, buff2, 10);
-    write(fd_fifo, buff, strlen(buff));
-    close(fd_fifo);
+    write(fd_fifo, buff, strlen(buff));					// делаем запись в канал
+    close(fd_fifo);							// закрываем его
+    exit(0);								// завершаем приложение с кодом 0(как return 0 в функции main)
 }
--- program3.c
+++ program3.c
@@ -11,33 +11,55 @@
 
 #define NOT_OPENED_FIFO1 1
 
+int sigint_event_handler(const char* pipe_name, const char* rm_fifo);
+
 int main(int argc, char* argv[])
 {
-    int fd1, num, prev_num = 0; // fd1 - дескриптор именнованного канала, num - число, которое будет в этом канале, prev_num - предыдущее число, которое было в этом канале (нужно для системы прерываний)
-    char buff[50]; // массив, в котором будут храниться символы из именнованного канала fifo
+    int fd1, num, prev_num = 0, is_first_app_closed = 0;		// fd1 - дескриптор именнованного канала, num - число, которое будет в этом канале,
+									// prev_num - предыдущее число, которое было в этом канале (нужно для системы прерываний)
+    char buff[50], buff2[5]; 						// массив, в котором будут храниться символы из именнованного канала fifo
     while(1)
     {
-	if((fd1 = open("fifo", O_RDONLY | O_NONBLOCK)) == -1) // проверка на открытие именнованного канала fifo (программа 1)
+	if((fd1 = open("fifo", O_RDONLY | O_NONBLOCK)) == -1) 		// проверка на открытие именнованного канала fifo (программа 1)
 	{
-	    fprintf(stderr, "Can't open fifo!\n"); // вывод об ошибке
+	    fprintf(stderr, "Can't open fifo!\n"); 			// вывод об ошибке
 	    break;
-	    return NOT_OPENED_FIFO1; // возвращение кода ошибки
+	    return NOT_OPENED_FIFO1; 					// возвращение кода ошибки
 	}
-	read(fd1, buff, 50); // запись данных с канала fifo в buff
-        num = atoi(buff); // перевод данных из char* в int
-	if(num == -1)
-	{
-	    close(fd1);
-	    unlink("fifo");
-	    fprintf(stdout, "First program was done...\n");
-	    return 0;
-	}
-        if(num != prev_num) // если, число внутри канала fifo изменилось, то выводим его в stdout, иначе ничего не делаем
+	read(fd1, buff, 50); 						// запись данных с канала fifo в buff
+        num = atoi(buff); 						// перевод данных из char* в int
+        if(num != prev_num) 						// если, число внутри канала fifo изменилось, то выводим его в stdout, иначе ничего не делаем
 	{
             fprintf(stdout, "%d\n", num);
-	    prev_num = num; // переприсваиваем prev_num
+	    prev_num = num; 						// переприсваиваем prev_num
 	}
-	close(fd1); // закрываем канал
+	close(fd1); 							// закрываем канал
+	is_first_app_closed = sigint_event_handler("fappexit", "fifo"); // вызываем функцию проверки закрытия 1ой программы
+	if(is_first_app_closed)						// если пришла 1
+	{
+	    fprintf(stdout, "First program was done...\n");		// выводим соответствующее сообщение
+	    break;							// выходим из бесконечного цикла
+	}
     }
     return 0;
+}
+
+
+int sigint_event_handler(const char* pipe_name, const char* rm_fifo) 	// функция, обрабатывающая завершение 1ой или 2ой программы (в зависимости от имени канала в pipe_name)
+{
+    int fd;								// дескриптор канала
+    char buff[5];							// хранилище данных в канале
+
+    if((fd = open(pipe_name, O_RDONLY | O_NONBLOCK)) != -1)		// если канал открылся
+    {
+        read(fd, buff, 5);						// считываем данные из канала в buff
+        if(atoi(buff) == -1)						// если данные == -1
+        {
+            close(fd);							// закрываем канал
+            unlink(pipe_name);						// удаляем канал(по имени)
+            unlink(rm_fifo);						// удаляем канал для чтения времени
+	    return 1;							// возвращаем 1, то есть приложение закрылось
+        }
+    }
+    return 0;								// если канал не открылся или там считалась не -1, возвращаем 0
 }
